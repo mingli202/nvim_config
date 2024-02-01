7bc1dac80324df60a5b146cf29a572ad1a5e4be6
Nanoscience202 <vincentmingli@gmail.com>
2024-01-31 21:24:27 -0500

    fix: neotree priority adjusted

--- lua/custom/plugins/neo_tree.lua
+++ lua/custom/plugins/neo_tree.lua
@@ -2,6 +2,7 @@ return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
   lazy = false,
+  priority = 999,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
