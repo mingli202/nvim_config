f419846bfae7bf601e5fea21ad3a85e1fcef3b5b
Nanoscience202 <vincentmingli@gmail.com>
2024-05-12 17:43:54 -0400

    updated plugins

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -40,9 +40,8 @@ return {
                 },
             },
             formatting = {
-                fields = { 'kind', 'abbr', 'menu' },
                 format = require('lspkind').cmp_format {
-                    mode = 'symbol', -- show only symbol annotations
+                    mode = 'symbol_text', -- show only symbol annotations
                     maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                     -- can also be a function to dynamically calculate max width such as
                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
