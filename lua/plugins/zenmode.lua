9a6930bf6787ac6c9fbd4a11862eda87f196c76a
mingli202 <vincentmingli@gmail.com>
2025-04-20 16:47:28 -0400

    removed unused plugins

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,13 +1,13 @@
 return {
-    'folke/zen-mode.nvim',
-    opts = {
-        plugins = {
-            options = {
-                laststatus = 3,
-            },
-        },
-    },
-    keys = {
-        { '<leader>z', ':ZenMode<CR>', desc = 'zen mode' },
-    },
+    -- 'folke/zen-mode.nvim',
+    -- opts = {
+    --     plugins = {
+    --         options = {
+    --             laststatus = 3,
+    --         },
+    --     },
+    -- },
+    -- keys = {
+    --     { '<leader>z', ':ZenMode<CR>', desc = 'zen mode' },
+    -- },
 }
