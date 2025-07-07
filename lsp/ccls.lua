7ace28abb9d7b700816976ae3b2f484431bcbe0b
mingli202 <vincentmingli@gmail.com>
2025-07-06 22:24:27 -0400

    ccls

---
+++ lsp/ccls.lua
@@ -0,0 +1,12 @@
+return {
+    init_options = {
+        clang = {
+            extraArgs = {
+                -- '-I/usr/local/include',
+                -- '-I/opt/homebrew/opt/llvm/lib/clang/**/include',
+                -- '-I/opt/homebrew/opt/llvm/include/c++/v1',
+                '-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include',
+            },
+        },
+    },
+}
