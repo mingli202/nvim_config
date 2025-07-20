c7e92b23d662f71fbfa1e3281f4e99c2c82d0be8
mingli202 <vincentmingli@gmail.com>
2025-07-19 22:47:05 -0400

    feat: added snacks and oil

--- lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'mbbill/undotree',
-    config = function()
-        local map = require('util').map
-        map('n', '<leader>u', ':UndotreeToggle <CR>', { desc = '[U]ndo tree' })
-    end,
-}
