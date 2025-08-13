746f73c6bca7d0bf519276d22132ad270a02ccf2
mingli202 <vincentmingli@gmail.com>
2025-08-13 18:29:10 -0400

    moved to lua folder

--- init.lua
+++ init.lua
@@ -691,7 +691,7 @@ require('lazy').setup {
         config = function()
             require('mini.ai').setup { n_lines = 500 }
             require('mini.surround').setup()
-            require('mini.pairs').setup()
+            -- require('mini.pairs').setup()
         end,
     },
 
