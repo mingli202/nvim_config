5b4bf714636a8c7ca88cc3feeb5d3bb536527f8b
mingli202 <vincentmingli@gmail.com>
2025-09-06 10:13:28 -0400

    hmmm

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,12 +3,15 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_enable_italic = 1
         vim.g.gruvbox_material_enable_bold = 0
         vim.g.gruvbox_material_better_performance = 1
         vim.g.gruvbox_material_transparent_background = not vim.g.neovide
-        vim.g.gruvbox_material_dim_inactive_windows = 1
+        vim.g.gruvbox_material_dim_inactive_windows = 0
         vim.g.gruvbox_material_inlay_hints_background = 'dimmed'
+        -- vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+        vim.g.gruvbox_material_diagnostic_line_highlight = 1
+        vim.g.gruvbox_material_diagnostic_text_highlight = 1
 
         vim.cmd.colorscheme 'gruvbox-material'
     end,
