f93d554df9777f7dca0c1958bf63cffb964ec3c2
mingli202 <vincentmingli@gmail.com>
2026-01-09 16:27:09 -0500

    updated luasnip config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,18 +6,10 @@ return { -- Autocompletion
         -- Snippet Engine
         {
             'L3MON4D3/LuaSnip',
-            version = '2.*',
-            build = (function()
-                -- Build Step is needed for regex support in snippets.
-                -- This step is not supported in many windows environments.
-                -- Remove the below condition to re-enable on windows.
-                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                    return
-                end
-
-                return 'make install_jsregexp'
-            end)(),
-            opts = {},
+            -- follow latest release.
+            version = 'v2.4', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = 'make install_jsregexp',
         },
         'folke/lazydev.nvim',
     },
