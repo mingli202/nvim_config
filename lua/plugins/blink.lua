2aa176842963db96dabc13faf247aa076ea6821c
mingli202 <vincentmingli@gmail.com>
2026-04-30 16:22:52 -0400

    added more snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -10,6 +10,15 @@ return { -- Autocompletion
             version = 'v2.4', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
             -- install jsregexp (optional!).
             build = 'make install_jsregexp',
+
+            dependencies = {
+                {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end,
+                },
+            },
         },
         'folke/lazydev.nvim',
     },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
             opts = {
                 registries = {
                     'github:mason-org/mason-registry',
-                    'github:Crashdummyy/mason-registry',
+                    -- 'github:Crashdummyy/mason-registry',
                 },
             },
         },
