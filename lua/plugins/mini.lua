30f31fbc249bb10389206a8bf6551304dc807ad7
mingli202 <vincentmingli@gmail.com>
2026-07-19 11:11:15 -0400

    mini statusline instead of lualine

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,6 +16,25 @@ return { -- Collection of various small independent plugins/modules
                 go_out_plus = '',
             },
         }
+        require('mini.statusline').setup {
+            content = {
+                active = function()
+                    local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+                    local filename = MiniStatusline.section_filename { trunc_width = 140 }
+                    local diff = MiniStatusline.section_diff { trunc_width = 75 }
+                    local git = MiniStatusline.section_git { trunc_width = 40 }
+                    local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+                    return MiniStatusline.combine_groups {
+                        { hl = mode_hl, strings = { mode } },
+                        { hl = 'MiniStatuslineFilename', strings = { filename } },
+                        '%=',
+                        { hl = 'MiniStatuslineFilename', strings = { diagnostics } },
+                        { hl = 'MiniStatuslineDevinfo', strings = { diff, git } },
+                        { hl = mode_hl, strings = { '%l|%L' } },
+                    }
+                end,
+            },
+        }
 
         local map = require('util').map
 
